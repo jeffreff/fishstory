70814109b2f1e46338468aaf3cf0481bed24c724
Jeffreff <U@^A$tP4Fvffp!>
2022-03-09 08:34:48 +0000

    first commit

---
+++ src/test/java/com/github/jeffreff/fishstory/FishstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jeffreff.fishstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
