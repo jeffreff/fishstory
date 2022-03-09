385fbf8ed9d81bbfda818da0274eac2ce864cf55
Jeffreff <U@^A$tP4Fvffp!>
2022-03-09 14:00:02 +0000

    FISHSTORY-24 #comment FishService deleteFishById()

--- src/test/java/com/github/jeffreff/fishstory/service/FishServiceUnitTest.java
+++ src/test/java/com/github/jeffreff/fishstory/service/FishServiceUnitTest.java
@@ -1,26 +1,20 @@
 package com.github.jeffreff.fishstory.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.any;
-
 import com.github.jeffreff.fishstory.domain.Fish;
 import com.github.jeffreff.fishstory.repo.FishRepo;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 public class FishServiceUnitTest {
@@ -72,8 +66,29 @@ public class FishServiceUnitTest {
         assertThat(result).isNotNull();
         assertThat(result).isEqualTo(testGuttedFish);
 
-
         verify(repo, times(1)).findById(id);
         verify(repo, times(1)).save(any(Fish.class));
+    }
+
+    @Test
+    public void deleteFishByIdTest() {
+        Long id = 1L;
+
+        when(repo.findById(id)).thenReturn(Optional.of(testFish));
+
+        this.service.deleteFishById(id);
+
+        verify(this.repo, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void deleteFishByIdFalseTest() {
+        Long id = 1L;
+
+        when(repo.findById(id)).thenReturn(Optional.empty());
+
+        this.service.deleteFishById(id);
+
+        verify(this.repo, times(0)).deleteById(id);
     }
 }
