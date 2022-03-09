66ec7d420f30e5c42b94479808c5f38da62554bb
Jeffreff <U@^A$tP4Fvffp!>
2022-03-09 10:41:39 +0000

    FISHSTORY-16 #comment FishController getFish()

--- src/main/java/com/github/jeffreff/fishstory/controller/FishController.java
+++ src/main/java/com/github/jeffreff/fishstory/controller/FishController.java
@@ -3,9 +3,12 @@ package com.github.jeffreff.fishstory.controller;
 import com.github.jeffreff.fishstory.domain.Fish;
 import com.github.jeffreff.fishstory.service.FishService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class FishController {
@@ -19,5 +22,10 @@ public class FishController {
     @PostMapping("/createFish")
     public Fish create(@RequestBody Fish newFish) {
         return this.service.create(newFish);
+    }
+
+    @GetMapping("/findFish")
+    public List<Fish> getFish() {
+        return this.service.readAll();
     }
 }
